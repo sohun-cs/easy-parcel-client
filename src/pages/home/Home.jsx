e227afa329be2ecc89ddf52f7e1e6838b4a818d6
AsaduzzamanSohun <asaduzzamansohun5@gmail.com>
2024-06-01 01:18:34 +0600

    EasyParcel Client

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="font-dongle text-7xl font-extrabold">EasyParcel</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/routes/router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layout/Root";
+import Home from "../pages/home/Home";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root></Root>,
+        children: [
+            {
+                index: true,
+                element: <Home></Home>
+            }
+        ]
+    }
+]);
+
+export default router;
